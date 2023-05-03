f92d86f6e920c8a45001e848eef4c0679eb22363
phyxius23 <30ntin@gmail.com>
2023-05-03 17:32:21 +0200

    exercise four completed

--- src/it/epicode/be/ExerciseFour.java
+++ src/it/epicode/be/ExerciseFour.java
@@ -5,55 +5,24 @@ import java.util.Scanner;
 public class ExerciseFour {
 
 	public static void main(String[] args) {
-		//rectanglePerimeter(); 		
-		oddEven();
-		perimeterAreaTriangle();
-	}
-	
-	/* Metodo PerimetroRettangolo: accetta la lunghezza decimale dei due lati di un rettangolo 
-	 * e ne calcola il perimetro
-	 */
-	public static void rectanglePerimeter() {
-		Scanner in = new Scanner(System.in);
-		
-		System.out.println("Inserisci il valore della base del rettangolo in decimale: ");
-		double width = in.nextDouble();
-		
-		System.out.println("Inserisci il valore dell'altezza del rettangolo in decimale: ");
-		double height = in.nextDouble();
-		
-		double perimeter = (width + height) * 2.0;
-		
-		System.out.println("Il perimetro del rettangolo è pari a: " + perimeter);
+    Scanner in = new Scanner(System.in);
 
-		in.close();
-	}
-	
-	/* Metodo PariDispari: accetta un numero intero e restituisce 0 se il numero è pari e 
-	 * 1 se è dispari
-	 */
-	public static void oddEven() {
-		Scanner in = new Scanner(System.in);
-		
-		System.out.println("Inserisci un numero intero: ");
-		int num = in.nextInt();		
-		
-		if((num % 2) == 1) {
-			System.out.println("Il numero inserito è dispari!");
-		} else {
-			System.out.println("Il numero inserito è pari!");
-		}
-		
-		in.close();
-	}
-	
-	/* Metodo PerimetroTriangolo: accetta le lunghezze decimali dei lati di un triangolo e ne 
-	 * restituisca l'area (è possibile usare la formula di Erone)
-	 */
-	public static void perimeterAreaTriangle() {
-		Scanner in = new Scanner(System.in);
-		
-		System.out.println("Inserisci la dimensione del lato 'a' del triangolo: ");
+    System.out.println("Inserisci il valore della base del rettangolo in decimale: ");
+    double width = in.nextDouble();
+    
+    System.out.println("Inserisci il valore dell'altezza del rettangolo in decimale: ");
+    double height = in.nextDouble();
+
+    double resultRectanglePerimeter = rectanglePerimeter(width, height);
+    System.out.println("Il perimetro del rettangolo è pari a: " + resultRectanglePerimeter);
+
+    System.out.println("Inserisci un numero intero: ");
+		int numberOddEven = in.nextInt();		
+
+    int resultOddEven = oddEven(numberOddEven);
+    System.out.println("Il numero inserito è " + (resultOddEven == 0 ? "pari" : "dispari"));
+
+    System.out.println("Inserisci la dimensione del lato 'a' del triangolo: ");
 		double sideOne = in.nextDouble();
 		
 		System.out.println("Inserisci la dimensione del lato 'b' del triangolo: ");
@@ -61,22 +30,35 @@ public class ExerciseFour {
 		
 		System.out.println("Inserisci la dimensione del lato 'c' del triangolo: ");
 		double sideThree = in.nextDouble();
-		
+
+    double resultAreaTriangle = areaTriangle(sideOne, sideTwo, sideThree);
+    System.out.println("L'area del triangolo è pari a " + resultAreaTriangle);
+
+    in.close();
+	}
+	
+	/* Metodo PerimetroRettangolo: accetta la lunghezza decimale dei due lati di un rettangolo 
+	 * e ne calcola il perimetro
+	 */
+	public static double rectanglePerimeter(double width, double height) {
+		return (width + height) * 2.0;
+	}
+	
+	/* Metodo PariDispari: accetta un numero intero e restituisce 0 se il numero è pari e 
+	 * 1 se è dispari
+	 */
+	public static int oddEven(int num) {
+    return num % 2 == 0 ? 0 : 1;
+	}
+	
+	/* Metodo PerimetroTriangolo: accetta le lunghezze decimali dei lati di un triangolo e ne 
+	 * restituisca l'area (è possibile usare la formula di Erone)
+	 */
+	public static double areaTriangle(double sideOne, double sideTwo, double sideThree) {
 		double semiPerimeter = (sideOne + sideTwo + sideThree) / 2;
-		
 		double rootArea = semiPerimeter * (semiPerimeter - sideOne) * (semiPerimeter - sideTwo) * (semiPerimeter - sideThree);
-		
-		double area = 0;
-		
-		if(rootArea >= 0) {
-			area = Math.sqrt(rootArea);
-			
-			System.out.println("L'area del triangolo è pari a: " + area);
-			System.out.println("Il perimetro del triangolo è pari a: " + (semiPerimeter * 2));
-		} else {
-			System.out.println("Area e perimetro del triangolo non possono essere calcolati dato che uno o più lati hanno un valore negativo");			
-		}
-		
-		in.close();
+		double area = Math.sqrt(rootArea);
+
+    return area;
 	}
 }
